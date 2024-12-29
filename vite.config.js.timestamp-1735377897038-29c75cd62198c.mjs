fdc0bf1604c18ee207cef8afe002dc2974fe3150
Harshavardhan Kurtkoti <kurtkoti.harsha@gmail.com>
2024-12-29 21:03:04 +0530

    timestamp files are added

---
+++ vite.config.js.timestamp-1735377897038-29c75cd62198c.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "file:///D:/threejs-portfolio-main/threejs-portfolio-main/node_modules/vite/dist/node/index.js";
+import react from "file:///D:/threejs-portfolio-main/threejs-portfolio-main/node_modules/@vitejs/plugin-react/dist/index.mjs";
+var vite_config_default = defineConfig({
+  plugins: [react()]
+});
+export {
+  vite_config_default as default
+};
