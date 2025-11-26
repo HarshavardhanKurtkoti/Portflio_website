35ca909988833561a3e435bf041200f4d94f72b2
Harshavardhan <kurtkoti.harsha@gmail.com>
2025-11-26 23:07:10 +0530

    feat: Add animated 3D target component with GSAP animation.

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,13 +1,9 @@
-import { useGLTF } from '@react-three/drei';
 import { useRef } from 'react';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 
 const Target = (props) => {
   const targetRef = useRef();
-  const { scene } = useGLTF(
-    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf',
-  );
 
   useGSAP(() => {
     gsap.to(targetRef.current.position, {
@@ -20,7 +16,8 @@ const Target = (props) => {
 
   return (
     <mesh {...props} ref={targetRef} rotation={[0, Math.PI / 5, 0]} scale={1.5}>
-      <primitive object={scene} />
+      <cylinderGeometry args={[0.1, 0.1, 4, 32]} />
+      <meshStandardMaterial color="#D0D3D4" />
     </mesh>
   );
 };
